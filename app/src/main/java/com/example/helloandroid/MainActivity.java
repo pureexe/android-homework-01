18d4433d4a9371832d11a92813597f07390d1d8c
Pakkapon Phongthawee <me@pureapp.in.th>
2017-09-03 23:27:51 +0700

    Initial commit

---
+++ app/src/main/java/com/example/helloandroid/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.helloandroid;
+/**
+ * @author ภัคพล พงษ์ทวี (07580028)
+ * @see https://github.com/pureexe/android-homework-01
+ */
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
